727b04c9ffb4d06c9e66b55665f8717506da07c2
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2024-11-16 00:12:15 +0300

    edit home page and bottomNavigationBar

--- lib/home.dart
+++ lib/home.dart
@@ -46,147 +46,156 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // Row for Text and Search Icon
-              Row(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // Row for Text and Search Icon
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'UCARE',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black87,
+                    fontSize: 40,
+                  ),
+                ),
+                IconButton(
+                  icon:
+                      const Icon(Icons.search, size: 30, color: Colors.black87),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SearchSreen()));
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey[300],
+              ),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text(
-                    'UCARE',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      color: Colors.black87,
-                      fontSize: 40,
-                    ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Herbal Clay\nPurifying Mask',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: null,
+                        child: Text('Shape Now',
+                            style: TextStyle(color: Colors.white)),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.black),
+                        ),
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.search, size: 30, color: Colors.black87),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SearchSreen()));
-                    },
+                  Transform.rotate(
+                    angle: -0.5,
+                    child: Image.asset(
+                      'assets/home-herbal.png',
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey[300],
+            ),
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('All', style: TextStyle(fontWeight: FontWeight.bold)),
+                Text('Cleaners'),
+                Text('Toner'),
+                Text('Essence'),
+                Text('Moisturizer'),
+              ],
+            ),
+            const SizedBox(height: 20),
+            // Grid for products
+            Expanded(
+              child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 0.7,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Herbal Clay\nPurifying Mask',
-                          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black87),
-                        ),
-                        SizedBox(height: 10),
-                        ElevatedButton(
-                          onPressed: null,
-                          child: Text('Shape Now', style: TextStyle(color: Colors.white)),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStatePropertyAll(Colors.black),
-                          ),
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return Container(
+                    margin: EdgeInsets.all(6),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          blurRadius: 5,
+                          spreadRadius: 3,
                         ),
                       ],
                     ),
-                    Transform.rotate(
-                      angle: -0.5,
-                      child: Image.asset(
-                        'assets/home-herbal.png',
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          product['image']!,
+                          height: 120,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          product['title']!,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          product['description']!,
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              product['price']!,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const Icon(
+                              Icons.favorite_border,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-              const SizedBox(height: 20),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text('All', style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text('Cleaners'),
-                  Text('Toner'),
-                  Text('Essence'),
-                  Text('Moisturizer'),
-                ],
-              ),
-              const SizedBox(height: 20),
-              // Grid for products
-              Expanded(
-                child: GridView.builder(
-                  itemCount: products.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: 20,
-                    childAspectRatio: 0.7,
-                  ),
-                  itemBuilder: (context, index) {
-                    final product = products[index];
-                    return Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.2),
-                            blurRadius: 5,
-                            spreadRadius: 3,
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(
-                            product['image']!,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            product['title']!,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            product['description']!,
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                          const Spacer(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                product['price']!,
-                                style: const TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              const Icon(
-                                Icons.favorite_border,
-                                color: Colors.grey,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
--- lib/menu_tab_bar.dart
+++ lib/menu_tab_bar.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:udaskinapp/home.dart';
 import 'package:udaskinapp/profil_screen.dart';
 import 'package:udaskinapp/wishlist_screen.dart';
 
 import 'cart_screen.dart';
 import 'chat_screen.dart';
-
 
 class MenuTabBar extends StatefulWidget {
   const MenuTabBar({super.key});
@@ -15,56 +15,61 @@ class MenuTabBar extends StatefulWidget {
 }
 
 class _MenuTabBarState extends State<MenuTabBar>
-with SingleTickerProviderStateMixin {
-  TabController? tabController;
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  int selectedIndex = 0;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     tabController = TabController(length: 5, vsync: this);
+
+    tabController.addListener(() {
+      if (tabController.indexIsChanging) {
+        setState(() {
+          selectedIndex = tabController.index;
+        });
+      }
+    });
   }
 
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TabBarView(
-        controller: tabController, children: const [
+        controller: tabController,
+        children: const [
           HomeScreen(),
           CartScreen(),
           WishlistScreen(),
           ChatScreen(),
           ProfilScreen(),
-      ]),
-    bottomNavigationBar: BottomAppBar(
-    child: TabBar(
-    labelColor: Colors.black,
-    unselectedLabelColor: Colors.grey,
-    controller: tabController,
-    tabs: const [
-      Tab(
-    text: 'Home',
-    icon: Icon(Icons.home),
-    ),
-    Tab(
-    text: 'Cart',
-    icon: Icon(Icons.card_travel  ),
-    ),
-    Tab(
-    text: 'WishList',
-    icon: Icon(Icons.favorite_outline),
-    ),
-      Tab(
-        text: 'Chat',
-        icon: Icon(Icons.mark_unread_chat_alt_outlined),
+        ],
       ),
-    Tab(
-    text: 'Profil',
-    icon: Icon(Icons.person),
-    ),
-    ],
-    ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (value) {
+          setState(() {
+            selectedIndex = value;
+            tabController.index = value;
+          });
+        },
+        selectedIndex: selectedIndex,
+        height: 55,
+        indicatorColor: const Color(0xffffffff),
+        backgroundColor: const Color(0xffffffff),
+        destinations: const [
+          NavigationDestination(icon: Icon(Ionicons.home_sharp), label: 'Home'),
+          NavigationDestination(icon: Icon(Ionicons.basket), label: 'Cart'),
+          NavigationDestination(icon: Icon(Ionicons.heart), label: 'WishList'),
+          NavigationDestination(icon: Icon(Ionicons.mail), label: 'Chat'),
+          NavigationDestination(icon: Icon(Ionicons.person), label: 'Profil'),
+        ],
       ),
     );
   }
